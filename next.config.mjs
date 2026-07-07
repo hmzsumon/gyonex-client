5d112467af85d2a91423efbf768c4531964aec15
Zakaria Sumon <zsumonn@gmail.com>
2026-07-07 19:27:15 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -26,7 +26,8 @@ const nextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "https://api-5-223-80-101.nip.io/api/v1/:path*",
+        destination:
+          "https://gyonex-api-8bd965ca374f.herokuapp.com/api/v1/:path*",
       },
     ];
   },
